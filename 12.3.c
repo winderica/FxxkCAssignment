8242d67ff69bea2bf7e0748f9353e07df5fe85b0
winderica <winderica@gmail.com>
2017-11-13 20:33:35 +0800

    javascript is the GREASTEST language in the worldgit add 12.3.cgit add 12.3.cgit add 12.3.cgit add 12.3.cgit add 12.3.cgit add 12.3.cgit add 12.3.cgit add 12.3.c!

---
+++ 12.3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void f(char a[], int i) {
+    if (a[i] == '\0') {
+        return;
+    }
+    f(a, i + 1);
+    printf("%c", a[i]);
+}
+
+int main() {
+    char a[1001];
+    while (scanf("%[^\n]%*c", a) !=  EOF) {
+        f(a, 0);
+        printf("\n");
+    }
+}
