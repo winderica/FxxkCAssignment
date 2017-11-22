9f88c1798006f5a98446bab35223d13597c4f7ae
winderica <winderica@gmail.com>
2017-11-22 21:33:01 +0800

    fix bug

--- c-ex/2-6.c
+++ c-ex/2-6.c
@@ -2,12 +2,14 @@
 int main()
 {
     long i;
+    int flag = 0;
     printf("input a number:");
     scanf("%ld", &i);
     while (i > 0) {
-	if (i % 10 != 0) {
+	if (i % 10 != 0 || flag) {
             printf("%ld", i % 10);
-        }
+	    flag = 1;
+	}
 	i /= 10;
     }
     printf("\n");
