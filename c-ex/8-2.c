04a1bdea5eea787673496b129bf48eaa57d94a72
winderica <winderica@gmail.com>
2017-12-30 23:19:13 +0800

    fix bugs

--- c-ex/8-2.c
+++ c-ex/8-2.c
@@ -16,4 +16,3 @@ int main(int argc, char * argv[]) {
     fclose(fp);
     return 0;
 }
-
--- c-ex/8-3.c
+++ c-ex/8-3.c
@@ -2,8 +2,20 @@
 int main() {
     char ch;
     FILE * fp;
+    int f = 1;
     fp = freopen("D:\\test.txt", "w", stdout);
-    while ((ch = getchar()) != '\n') putchar(ch); // 加括号
+    while ((ch = getchar()) != '\n') {
+        if (f) {
+            if (ch >= 97 && ch <= 122) {
+                ch -= 32;
+            }
+            f = 0;
+        }
+        if (ch == ' ') {
+            f = 1;
+        }
+        putchar(ch);
+    }
     fclose(fp);
     return 0;
 }
